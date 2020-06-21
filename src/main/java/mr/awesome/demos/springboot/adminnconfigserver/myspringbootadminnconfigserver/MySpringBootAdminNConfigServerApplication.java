14123ced5bfc4a933a6f4b4ea816505334ee5c0e
Arjun <>
2020-06-21 10:49:27 +0530

    Missing dependencies
    Signed-off-by: Arjun <>
    
     Date:      Sun Jun 21 10:49:27 2020 +0530
     On branch master
     Changes to be committed:
    	modified:   build.gradle
    	modified:   src/main/java/mr/awesome/demos/springboot/adminnconfigserver/myspringbootadminnconfigserver/MySpringBootAdminNConfigServerApplication.java

--- src/main/java/mr/awesome/demos/springboot/adminnconfigserver/myspringbootadminnconfigserver/MySpringBootAdminNConfigServerApplication.java
+++ src/main/java/mr/awesome/demos/springboot/adminnconfigserver/myspringbootadminnconfigserver/MySpringBootAdminNConfigServerApplication.java
@@ -1,8 +1,12 @@
 package mr.awesome.demos.springboot.adminnconfigserver.myspringbootadminnconfigserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
+@EnableAdminServer
 @SpringBootApplication
 public class MySpringBootAdminNConfigServerApplication {
 
